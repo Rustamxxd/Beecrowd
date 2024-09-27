dc3f308198fc1fdafddfade9e3ed4ac706c43ee7
Rustamxxd <125432441+Rustamxxd@users.noreply.github.com>
2024-09-27 17:43:21 +0600

    Update Bee1193.java

--- src/Bee1193.java
+++ src/Bee1193.java
@@ -1,29 +1,36 @@
 import java.util.Scanner;
 
-public class Bee1193 {
+public class Main {
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt();
         scanner.nextLine();
 
         for (int i = 1; i <= n; i++) {
-            String input = scanner.nextLine().trim();
-            String[] parts = input.split(" ");
-            int number;
-            String base = parts[1];
+            String[] input = scanner.nextLine().split(" ");
+            String number = input[0];
+            String format = input[1];
 
-            if (base.equals("bin")) {
-                number = Integer.parseInt(parts[0],2);
-            } else if (base.equals("dec")) {
-                number = Integer.parseInt(parts[0]);
-            } else {
-                number = Integer.parseInt(parts[0],16);
+            System.out.println("Case " + i + ":");
+            
+            switch (format) {
+                case "bin":
+                    int decimalFromBin = Integer.parseInt(number, 2);
+                    System.out.println(decimalFromBin + " dec");
+                    System.out.println(Integer.toHexString(decimalFromBin)+ " hex");
+                    break;
+                case "dec":
+                    int decimal = Integer.parseInt(number);
+                    System.out.println(Integer.toHexString(decimal) + " hex");
+                    System.out.println(Integer.toBinaryString(decimal)+ " bin");
+                    break;
+                case "hex":
+                    int decimalFromHex = Integer.parseInt(number, 16);
+                    System.out.println(decimalFromHex + " dec");
+                    System.out.println(Integer.toBinaryString(decimalFromHex)+ " bin");
+                    break;
             }
-
-            System.out.println("Test Case " + i + ":");
-            System.out.println(number);
-            System.out.println(Integer.toHexString(number));
-            System.out.println(Integer.toBinaryString(number));
             System.out.println();
         }
         scanner.close();
